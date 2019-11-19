17b1d85ecb7d61deb919687177ff64bffde0ca95
Rifky Pratama <47963353+tamarifky25@users.noreply.github.com>
2019-11-19 08:36:16 +0700

    Add files via upload

---
+++ Website Project/assets/js/script.js
@@ -0,0 +1,55 @@
+function onHover(){
+  $('#img1').attr('src', 'assets/img/72ppi/Asset 82.png');
+}
+function offHover(){
+  $('#img1').attr('src', 'assets/img/72ppi/Asset 51.png');
+}
+
+function onHover2(){
+  $('#img2').attr('src', 'assets/img/72ppi/Asset 83.png');
+}
+function offHover2(){
+  $('#img2').attr('src', 'assets/img/72ppi/Asset 53.png');
+}
+
+function onHover3(){
+  $('#img3').attr('src', 'assets/img/72ppi/Asset 84.png');
+}
+function offHover3(){
+  $('#img3').attr('src', 'assets/img/72ppi/Asset 55.png');
+}
+
+function onHover4(){
+  $('#img4').attr('src', 'assets/img/72ppi/Asset 85.png');
+}
+function offHover4(){
+  $('#img4').attr('src', 'assets/img/72ppi/Asset 60.png');
+}
+
+function onHoverG1(){
+  $('#img-g1').attr('src', 'assets/img/72ppi/Asset1.png');
+}
+function offHoverG1(){
+  $('#img-g1').attr('src', 'assets/img/72ppi/Asset2.png');
+}
+
+function onHoverG2(){
+  $('#img-g2').attr('src', 'assets/img/72ppi/Asset4.png');
+}
+function offHoverG2(){
+  $('#img-g2').attr('src', 'assets/img/72ppi/Asset3.png');
+}
+
+function onHoverG3(){
+  $('#img-g3').attr('src', 'assets/img/72ppi/Asset6.png');
+}
+function offHoverG3(){
+  $('#img-g3').attr('src', 'assets/img/72ppi/Asset5.png');
+}
+
+function onHoverG4(){
+  $('#img-g4').attr('src', 'assets/img/72ppi/Asset8.png');
+}
+function offHoverG4(){
+  $('#img-g4').attr('src', 'assets/img/72ppi/Asset7.png');
+}
